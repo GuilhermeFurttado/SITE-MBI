7311b12b97a1fb7f9388f0a1be702370cdece98f
GuilhermeFurttado <guibaterista1313@gmail.com>
2022-09-05 13:35:15 -0300

    site

--- mail/contact.php
+++ mail/contact.php
@@ -9,11 +9,11 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "mbisolutions2022@gmail.com";
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$body = "Você recebeu uma nova mensagem da MBI Solutions.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\n\nEmail: $email\n\nDestino: $m_subject\n\nMensagem: $message";
+$header = "Para: $email";
+$header .= "Responder para: $email";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
